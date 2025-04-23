4c4fbb18f4ac2dd4e0a99e2defc47590b5e59737
Jeffery Pereira <jefferypereira3@gmail.com>
2025-04-23 13:31:24 -0500

    Adding destroy version of code

--- main_vn.tf
+++ main_vn.tf
@@ -34,24 +34,3 @@ provider "azurerm" {
 
 # RESOURCES
 
-module "vnet-main" {
-	source 							= "Azure/vnet/azurerm"
-	resource_group_name = var.resource_group_name
-	vnet_location 			= var.location
-	vnet_name						= var.resource_group_name
-	address_space				= var.vnet_cidr_range
-	subnet_prefixes			= var.subnet_prefixes
-	subnet_names				= var.subnet_names
-	nsg_ids							= {}
-	
-	tags = {
-		environment = "dev"
-		costcenter	= "it"
-	}
-}
-
-# OUTPUTS
-
-output "vnet_id" { 
-	value = module.vnet-main.vnet_id
-}
